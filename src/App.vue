0b86b864b2f694560d4db19e1ae664cd3d41c5e7
Hector Miguel Reynaga Aregullin <hmaregullin@gmail.com>
2023-11-20 14:45:31 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Navbar/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './components/Navbar.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Navbar
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Hello world, @hectormartialarts here</p>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Navbar'
+}
+</script>
